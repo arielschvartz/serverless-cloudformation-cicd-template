005235a1030d960a3c78c87cadcced9433266650
Ariel Schvartz <ari.shh@gmail.com>
2020-08-12 11:45:02 -0300

    Fixed problem

--- .serverless_plugins/custom-resolvers.js
+++ .serverless_plugins/custom-resolvers.js
@@ -14,6 +14,7 @@ class ParentServiceNameResolver {
     this.variableResolvers = {
       parent: this.parentServiceNameResolver,
       escapedFileToString: this.escapedFileToStringResolver,
+      ymlToString: this.ymlToStringResolver,
       fileToString: this.fileToStringResolver,
     }
   }
